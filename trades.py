157f3f1e3faeb193c843650842bd28c9818ecf8a
kiranikram <kiran.ikram91@gmail.com>
2022-12-05 13:01:10 +0000

    last task done

--- trades.py
+++ trades.py
@@ -51,7 +51,18 @@ def get_product(df,product):
 
 # TODO for each contract return ohlc
 
-def main_func(df,day,begin,end,product,freq):
+def main_func(df,day,product,freq,begin=7,end=17):
+    """Args: df: original trades df,
+            day: int(day out of working days provided)
+            product: str(product name)
+            freq: str(15T / 1H / 1D)
+            begin: int(start of trading day)
+            end: int(end of trading day)
+
+       Returns: Dict of dataframes for time period specified. Each dataframe in the dictionary 
+                corresponds to a unique contract for the product that is queried. 
+    """
+    df = get_interval(df,day)
     product_ohlc = {}
     product_segments = df.groupby(df['Product'])
     product_df = product_segments.get_group(product)
@@ -61,7 +72,24 @@ def main_func(df,day,begin,end,product,freq):
 
     for contract in contracts:
         contract_df = contract_segments.get_group(contract)
-        contract_df = contract_df.set_index('Datetime')
+        df = contract_df.set_index('TradeDateTime')
+        open_df = df.resample(freq).agg({'Price': 'first'}) 
+        high_df = df.resample(freq).agg({'Price': 'max'}) 
+        low_df = df.resample(freq).agg({'Price': 'min'}) 
+        close_df = df.resample(freq).agg({'Price': 'last'}) 
+        volume_df = df.resample(freq).agg({'Price': 'sum'})
+        con_df =pd.concat([open_df, high_df, low_df, close_df, volume_df], axis=1, keys=['open', 'high', 'low', 'close', 'volume'])
+        if freq != '1D':
+            df = con_df.reset_index()
+            filt = (df['TradeDateTime'].dt.hour >= begin) & (df['TradeDateTime'].dt.hour <= end)
+
+            con_df = df.loc[filt, :]
+
+        con_df = con_df.dropna()
+
+        product_ohlc[contract] = con_df
+
+    return product_ohlc
 
 
 
